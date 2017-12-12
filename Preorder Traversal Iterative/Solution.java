ff92d65952d096b67154112c6f0001c0fed6c65d
kprav <kprav@users.noreply.github.com>
2017-12-11 16:22:13 -0800

    Create Solution.java

---
+++ Preorder Traversal Iterative/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode a) {
+        if (a == null) {
+            return null;
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(a);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+}
